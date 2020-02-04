5e105626262c7bb96a5d798b7eb217151d2efc46
mehdigoom <medhi.gay@sfr.fr>
2020-02-04 14:47:05 +0100

    Add basic server

---
+++ server.js
@@ -0,0 +1,40 @@
+function Server(opt) {
+    this.opt = opt;
+    this.httpServer = require('http').createServer();
+    this.io = require('socket.io')(this.httpServer)
+    this.start();
+}
+Server.prototype.start = function() {
+
+    console.log("Runnig server")
+    
+that = this;
+   
+
+
+//Player connecting
+   this.io.on('connection', function(socket) {
+    //on connection
+           
+        socket.on('disconnect', function() {
+//disconect client
+        });
+
+    });
+    //Runnig server
+    this.httpServer.listen(this.opt.port, function() {
+        console.log("le server écoute le port: " + that.opt.port )
+
+    });
+
+
+
+
+
+
+
+
+
+
+};
+module.exports = Server;
---
+++ start.js
@@ -0,0 +1,6 @@
+var args = {
+    port: 3000
+};
+
+var Server = require('./server.js');
+var server = new Server(args);
